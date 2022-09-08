fd4234e70b40fe0b037c7afc15949cb43219d122
Serhii Malyshko <sergiy.malyshko@codegenerationsoftware.com>
2022-09-08 11:31:02 +0300

    feat: added User module

---
+++ src/user/user.controller.ts
@@ -0,0 +1,20 @@
+import {
+  Controller,
+  Get,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
+
+@Controller('users')
+export class UserController {
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getMe(@Req() req: Request) {
+    console.log({
+      user: req.user,
+    });
+    return req.user;
+  }
+}
